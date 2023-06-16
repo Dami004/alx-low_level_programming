b01b62077a651ad9232fe1dfe5953a9874cfd68e
Temitope <topzenoni@gmail.com>
2023-06-16 01:50:57 -0700

    re

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,7 +11,6 @@ int main(void)
 	long int c;
 	long long int d;
 	float f;
-
 printf("size of a char: %d byte(s)\n", (unsigned long)sizeof(a));
 printf("size of an int: %d byte(s)\n", (unsigned long)sizeof(b));
 printf("size of a long int: %d byte(s)\n", (unsigned long)sizeof(c));
