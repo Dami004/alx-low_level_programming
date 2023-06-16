f7ff8bdbc93f6d72d20cbd12a197c76b7e322a3d
Temitope <topzenoni@gmail.com>
2023-06-15 18:53:42 -0700

    f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - A alx c program that prints a line using printf function with betty
- * return: 0
- **/
+ * return: Always 0 (success)
+ */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
