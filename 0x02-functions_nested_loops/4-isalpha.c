1da49aba7babe34bfd7ebd49485c6279896d9b5a
Temitope <topzenoni@gmail.com>
2023-06-21 03:46:15 -0700

    c functions

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,2 +1,19 @@
 #include "main.h"
+/**
+ * _isalpha - Checks for alphabetic characters
+ * @c: The character to be checked
+ * Return: 1 for alphabetic character or 0 for anything else
+ */
 
+int _isaplha(int c);
+{
+	if ((c >= 97; c <= 122) || (c >= 65 && c <= 99))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
