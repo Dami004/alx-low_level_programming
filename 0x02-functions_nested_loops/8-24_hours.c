e5421ad93cac46815ca9a59bbc681c1fce864cd0
Temitope <topzenoni@gmail.com>
2023-06-21 10:16:15 -0700

    functions

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -18,10 +18,10 @@ void jack_bauer(void)
 				for (d = 0; d <= 9; d++)
 				{
 					if (a >= 2 && b >= 0)
-						break:
+						break;
 					_putchar(a + 48);
 					_putchar(b + 48);
-					_putchar(18);
+					_putchar(50);
 					_putchar(c + 48);
 					_putchar(d + 48);
 					_putchar('\n');
