9573c2086f3ea13c57adb268e0d179ef237053a2
Temitope <topzenoni@gmail.com>
2023-06-26 09:15:57 -0700

    pointers

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,13 +3,12 @@
 /**
  * _puts - Prints a string .
  * @str: string.
- * Return: Always 0
  */
 void _puts(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0' ; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		_putchar(str[i]);
 	}
