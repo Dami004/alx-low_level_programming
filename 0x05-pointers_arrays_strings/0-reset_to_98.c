cb6e5a5ddb061b2a688cbe30af9f925d17c54132
Temitope <topzenoni@gmail.com>
2023-06-26 08:31:24 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Update the pointer value
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int b = 402;
+	int *n = &b;
+
+	*n = 98;
+
+	printf("%d\n", n);
+
+	return (0);
+}
